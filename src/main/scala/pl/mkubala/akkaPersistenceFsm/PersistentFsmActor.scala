8fca40939804df65de356cb8dccb5613de61c672
Marcin Kubala <marcin@mkubala.pl>
2019-01-08 22:05:41 +0100

    Add constraints for Add & Subtract comands

--- src/main/scala/pl/mkubala/akkaPersistenceFsm/PersistentFsmActor.scala
+++ src/main/scala/pl/mkubala/akkaPersistenceFsm/PersistentFsmActor.scala
@@ -15,8 +15,12 @@ case object Negative extends MyState {
 
 sealed trait MyCommand
 case object Increment extends MyCommand
-case class Add(n: Int) extends MyCommand
-case class Subtract(n: Int) extends MyCommand
+case class Add(n: Int) extends MyCommand {
+  require(n >= 0, "number must be >= 0")
+}
+case class Subtract(n: Int) extends MyCommand {
+  require(n >= 0, "number must be >= 0")
+}
 
 sealed trait MyEvent
 case object Incremented extends MyEvent
